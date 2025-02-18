0ca9f0d39e9654156d3b0abc34283c6438ec0b83
yanalec <yanalec448@gmail.com>
2025-02-18 10:56:39 -0300

    First commit

---
+++ backend/src/main/java/com/bolsaPackage/bolsaNav/BolsaNavApplication.java
@@ -0,0 +1,13 @@
+package com.bolsaPackage.bolsaNav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsaNavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolsaNavApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bolsaPackage/bolsaNav/BolsaNavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsaPackage.bolsaNav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsaNavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
